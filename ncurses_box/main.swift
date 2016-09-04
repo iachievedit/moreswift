81d3f65617be2f60e65e98d7590d62869c20eb51
Joseph Bell <joe@iachieved.it>
2016-09-04 13:39:07 +0000

    @escaping

--- ncurses_box/main.swift
+++ ncurses_box/main.swift
@@ -9,7 +9,7 @@ case WINCH = 28
 
 typealias SignalHandler = __sighandler_t
 
-func trap(signum:Signal, action:SignalHandler) {
+func trap(signum:Signal, action:@escaping SignalHandler) {
   signal(signum.rawValue, action)
 }
 
